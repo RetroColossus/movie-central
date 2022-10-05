8131b9d9355a06a4325b7a23be8cc78cb6a0c4af
Jeff Small <jeffmeister1990@gmail.com>
2022-10-04 21:03:58 -0400

    Created global state sample

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -6,4 +6,32 @@ import {
     UPDATE_MOVIE_PREFERENCES
 } from '../utils/actions';
 
+// import reducer
+import { reducer } from '../utils/reducers';
 
+// create a sample of what our global state will look like
+const initialState = {
+    currentUser: 111,
+    likedMovies: [
+        {
+            "externalMovieId": 1,
+            "overview": "This is Grumpy Cat's movie",
+            "title": "Grumpy Cat"
+        }
+    ],
+    dislikedMovies: [
+        {
+            "externalMovieId": 2,
+            "title": "The Trial of the Chicago 7",
+            "overview": "What was intended to be a peaceful protest at the 1968"
+
+        }
+    ],
+    movies: [
+        {
+            "externalMovieId": 3,
+            "title": "Love and Monsters",
+            "overview": "Seven years after the Monsterpocalypse"
+        }
+    ]
+};
