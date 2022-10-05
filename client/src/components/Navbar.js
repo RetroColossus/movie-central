5dafe3659a3e9daf45a01b849f987711f27aaeb4
Jeff Small <jeffmeister1990@gmail.com>
2022-10-04 21:11:36 -0400

    Created imports for NavBar.js

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Modal, Tab } from 'react-bootstrap';
+import SignUpForm from './SignupForm';
+import LoginForm from './LoginForm';
